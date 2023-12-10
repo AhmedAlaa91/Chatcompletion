bac6f26007d34dfa36f1af9c1ae1082fe56ea822
AhmedAlaa91 <49675734+AhmedAlaa91@users.noreply.github.com>
2023-12-10 10:12:28 +0200

    Update main.py

--- ChatCompletion/main.py
+++ ChatCompletion/main.py
@@ -1,12 +1,16 @@
 from decouple import config
 import openai
 
+#pass api key from enviroment variables to variable API_KEY
 API_KEY = config('OPENAI_KEY')
 
+#stablish connection with openAI client
 client = openai.OpenAI(api_key=API_KEY)
 
+#wait for user message prompt
 message = input ("Enter Your Message : ")
 
+#send instance to chat completion api
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
